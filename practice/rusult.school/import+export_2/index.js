47d84d80fb7965638f8b89f45bb18aef81461614
cultkkari <osv1313006@gmail.com>
2023-10-29 14:49:13 +0300

    task 2

--- practice/rusult.school/import+export_2/index.js
+++ practice/rusult.school/import+export_2/index.js
@@ -1,9 +1,17 @@
 import './index.css';
 
 const btn = document.querySelector('.cookie-consent__button')
+const textDiv = document.querySelector('.cookie-consent');
+const isButtonClicked = localStorage.getItem('isButtonClicked');
+
+if (isButtonClicked) {
+    textDiv.classList.add('hide');
+} else {
+    textDiv.classList.remove('hide');
+}
 
 btn.addEventListener('click', (event) => {
     event.preventDefault();
-
-
+    textDiv.classList.add('hide');
+    localStorage.setItem('isButtonClicked', true)
 })
