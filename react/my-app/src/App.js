b37b13f9b1bfa75122c5126abd6daaf4024d0b1b
cultkkari <osv1313006@gmail.com>
2023-11-08 19:46:36 +0300

    mrth

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,42 +1,29 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { getCurrentDate } from './utils';
 
 function App() {
-	const appDiv = React.createElement(
-		'div',
-		{ className: 'App' },
-		React.createElement(
-			'header',
-			{ className: 'App-header' },
-			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-			React.createElement(
-				'p',
-				null,
-				'Edit ',
-				React.createElement('code', null, 'src/App.js'),
-				' and save to reload.',
-			),
-			React.createElement(
-				'a',
-				{
-					className: 'App-link',
-					href: 'https://reactjs.org',
-					target: '_blank',
-					rel: 'noopener noreferrer',
-				},
-				'Learn React',
-			),
-			React.createElement(
-				'a',
-				{ href: '#', className: 'App-link' },
-				getCurrentDate(),
-			),
-		),
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<a href="#" className="App-link">
+					{getCurrentDate()}
+				</a>
+			</header>
+		</div>
 	);
-
-	return appDiv;
 }
 
 export default App;
