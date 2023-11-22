415f2c7d95263a8bed53319656c6ab8cf201771a
cultkkari <osv1313006@gmail.com>
2023-11-22 20:28:53 +0300

    tictac

--- tic-tac-toe/src/components/Filed/Field.jsx
+++ tic-tac-toe/src/components/Filed/Field.jsx
@@ -1,40 +1,71 @@
-import styles from './Field.module.css'
-import Square from "../Square/Square.jsx";
-import {useState} from "react";
+import styles from './Field.module.css';
+import Square from '../Square/Square.jsx';
+import { useState } from 'react';
 
 const Field = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
+	const [squares, setSquares] = useState(Array(9).fill(null));
+	const [xIsNext, setXIsNext] = useState(true);
 
-    function handleClick(i) {
-        if (squares[i]) {
-            return;
-        }
-        const nextSquares = squares.slice();
-        if (xIsNext) {
-            nextSquares[i] = 'X';
-        } else {
-            nextSquares[i] = 'O';
-        }
-        setSquares(nextSquares);
-        setXIsNext(!xIsNext)
-    }
+	function handleClick(i) {
+		if (squares[i] || calculateWinner(squares)) {
+			return;
+		}
+		const nextSquares = squares.slice();
+		if (xIsNext) {
+			nextSquares[i] = 'X';
+		} else {
+			nextSquares[i] = 'O';
+		}
+		setSquares(nextSquares);
+		setXIsNext(!xIsNext);
+	}
 
-    return (
-        <>
-            <div className={styles.field}>
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-            </div>
-        </>
-    )
-}
+	const winner = calculateWinner(squares);
+	let status;
+	if (winner) {
+		status = 'Победитель: ' + winner;
+	} else {
+		status = 'Следующий ход ' + (xIsNext ? 'X' : 'O');
+	}
 
-export default Field
+	const startNewGame = () => {
+		return (
+			<button className={styles.newGame} onClick={() => setSquares(Array(9).fill(null))}> Начать заново</button>
+		);
+	};
+	return (
+		<>
+			<div className={styles.status}>{status}</div>
+			{startNewGame()}
+			<div className={styles.field}>
+				{
+					squares.map((square, i) => (
+						<Square key={i} value={square} onSquareClick={() => handleClick(i)} />
+					))
+				}
+			</div>
+		</>
+	);
+};
+
+const calculateWinner = (squares) => {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
+};
+
+export default Field;
